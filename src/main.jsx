36c4e6b23be9427bd8a78f0c0dd060a99028d7d2
Anjala <120647099+Anjala125@users.noreply.github.com>
2026-01-10 22:09:43 +0530

    Refactor main.jsx to use BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
 const redirect = sessionStorage.redirect;
 if (redirect) {
   sessionStorage.removeItem("redirect");
   window.history.replaceState(null, null, redirect);
 }
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )

35f10b5cf53637ca8f11a02387d27654fd69b55b
Anjala <120647099+Anjala125@users.noreply.github.com>
2026-01-10 21:56:30 +0530

    Implement redirect handling in main.jsx
    Add redirect handling from sessionStorage before rendering the app.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, null, redirect);
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
